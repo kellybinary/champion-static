0746a4bd400d9c5db8e34e7e9c1622485631f3fc
kelly <kelly@binary.com>
2017-08-24 23:24:28 +0800

    Auto-generated commit

--- br_header/js/bundle.js
+++ br_header/js/bundle.js
@@ -24616,6 +24616,7 @@
 	            $('.navbar__brand, .navbar__toggle').removeClass('logged-in'); // show logo
 	            $('#header').removeClass('navbar--fixed');
 	        }
+	        $('body').scrollTop(1);
 	        $('.navbar__nav').height($(window).innerHeight());
 	        $(window).on('orientationchange resize', function () {
 	            $('.navbar__nav').height($(window).innerHeight());
